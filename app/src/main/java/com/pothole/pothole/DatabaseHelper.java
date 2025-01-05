224b18e2454913ce68dcd6cdcd13c9b757ea47c6
YouQuinney <myquyen.19092003@gmail.com>
2025-01-05 13:48:07 +0700

    Update DatabaseHelper.java

--- app/src/main/java/com/pothole/pothole/DatabaseHelper.java
+++ app/src/main/java/com/pothole/pothole/DatabaseHelper.java
@@ -16,7 +16,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_3 = "LATITUDE";
     public static final String COL_4 = "LONGITUDE";
     public static final String T2COL_1 = "USERNAME";
-    public static final String T2COL_2 = "POTHOLENUMBER";
+    public static final String T2COL_2 = "PASSWORD";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -25,13 +25,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,POTHOLE_NUMBER TEXT,LATITUDE TEXT,LONGITUDE TEXT)");
+        db.execSQL("create table " + USER_TABLE +" (USERNAME TEXT PRIMARY KEY,PASSWORD TEXT )");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS "+USER_TABLE);
         onCreate(db);
     }
+
+    public boolean isUsernameTaken(String username) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + USER_TABLE + " WHERE USERNAME = ?", new String[]{username});
+        boolean exists = cursor.getCount() > 0;
+        cursor.close();
+        return exists;
+    }
+
 
     public boolean insertData(String pothole_number,String latitude,String longitude) {
         SQLiteDatabase db = this.getWritableDatabase();
@@ -46,17 +58,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return true;
     }
 
-    public boolean insertUser(String username, String potholeno){
+    public boolean insertUser(String username, String password) {
+        if (isUsernameTaken(username)) {
+            return false;
+        }
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(T2COL_1,username);
-        contentValues.put(T2COL_2,potholeno);
-        long result = db.insert(USER_TABLE,null ,contentValues);
-        if(result == -1)
-            return false;
-        else
-            return true;
+        contentValues.put(T2COL_1, username);
+        contentValues.put(T2COL_2, password);
+        long result = db.insert(USER_TABLE, null, contentValues);
+        return result != -1;
     }
+
 
     public Cursor getAllData() {
         SQLiteDatabase db = this.getWritableDatabase();
@@ -68,6 +81,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor res = db.rawQuery("select * from "+USER_TABLE,null);
         return res;
+    }
+
+    public boolean checkLogin(String username, String password) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(
+                "SELECT * FROM " + USER_TABLE + " WHERE USERNAME = ? AND PASSWORD = ?",
+                new String[]{username, password}
+        );
+        boolean isValid = cursor.getCount() > 0;
+        cursor.close();
+        return isValid;
     }
 
 }
