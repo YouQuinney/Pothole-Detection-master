966e2f2423008d5b620f12379d77fa72137ec3d2
YouQuinney <myquyen.19092003@gmail.com>
2025-01-05 13:49:08 +0700

    Update MQTTHelper.java

--- app/src/main/java/com/pothole/pothole/MQTTHelper.java
+++ app/src/main/java/com/pothole/pothole/MQTTHelper.java
@@ -1,5 +1,90 @@
+//package com.pothole.pothole;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.util.Log;
+//
+//import org.eclipse.paho.android.service.MqttAndroidClient;
+//import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+//import org.eclipse.paho.client.mqttv3.IMqttToken;
+//import org.eclipse.paho.client.mqttv3.MqttClient;
+//import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+//import org.eclipse.paho.client.mqttv3.MqttException;
+//import org.eclipse.paho.client.mqttv3.MqttMessage;
+//
+//import java.io.UnsupportedEncodingException;
+//
+//import static android.content.ContentValues.TAG;
+//
+//public class MQTTHelper extends AppCompatActivity {
+//
+//
+//    public void connect(MqttAndroidClient client, String userName, String password) {
+//        MqttConnectOptions options = new MqttConnectOptions();
+//        options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
+//        options.setUserName(userName);
+//        options.setPassword(password.toCharArray());
+//        try {
+//            IMqttToken token = client.connect(options);
+//            token.setActionCallback(new IMqttActionListener() {
+//                @Override
+//                public void onSuccess(IMqttToken asyncActionToken) {
+//                    // We are connected
+//                    Log.d(TAG, "onSuccess");
+//                }
+//
+//                @Override
+//                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+//                    // Something went wrong e.g. connection timeout or firewall problems
+//                    Log.d(TAG, "onFailure");
+//
+//                }
+//            });
+//        } catch (MqttException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//    public void subscribe(MqttAndroidClient client) {
+//        String subtopic = "Android";
+//        int qos = 1;
+//        try {
+//            IMqttToken subToken = client.subscribe(subtopic, qos);
+//            subToken.setActionCallback(new IMqttActionListener() {
+//                @Override
+//                public void onSuccess(IMqttToken asyncActionToken) {
+//                    // The message was published
+//                    Log.i("mqtt", "subscription success");
+//                }
+//
+//                @Override
+//                public void onFailure(IMqttToken asyncActionToken,
+//                                      Throwable exception) {
+//                    // The subscription could not be performed, maybe the user was not
+//                    // authorized to subscribe on the specified topic e.g. using wildcards
+//                    Log.i("mqtt", "subscription failed");
+//
+//                }
+//            });
+//        } catch (MqttException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//    public void publish(MqttAndroidClient client, String topic, String payload) {
+//        byte[] encodedPayload = new byte[0];
+//        try {
+//            encodedPayload = payload.getBytes("UTF-8");
+//            MqttMessage message = new MqttMessage(encodedPayload);
+//            client.publish(topic, message);
+//        } catch (UnsupportedEncodingException | MqttException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//}
+
 package com.pothole.pothole;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -11,18 +96,38 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
 import java.io.UnsupportedEncodingException;
 
 import static android.content.ContentValues.TAG;
 
 public class MQTTHelper extends AppCompatActivity {
 
+    String URL = "ssl://8f9715ee0305436cb77e65999dc5b212.s1.eu.hivemq.cloud";
+    String portNumber = "8883";
+    String userName = "vbqcvuri";
+    String mqpassword = "tgt22oGl7EwE";
+    ///////////////////////////////////////////////////////////////////////////
+    String clientId = MqttClient.generateClientId();
+    private SSLSocketFactory createSocketFactory() {
+        try {
+            SSLContext context = SSLContext.getInstance("TLSv1.2");
+            context.init(null, null, null);
+            return context.getSocketFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-    public void connect(MqttAndroidClient client, String userName, String password) {
+    public MqttAndroidClient connect(Context context) {
+        MqttAndroidClient client =new MqttAndroidClient(context,URL+":"+portNumber,clientId);
         MqttConnectOptions options = new MqttConnectOptions();
         options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
         options.setUserName(userName);
-        options.setPassword(password.toCharArray());
+        options.setPassword(mqpassword.toCharArray());
+        options.setSocketFactory(createSocketFactory());
         try {
             IMqttToken token = client.connect(options);
             token.setActionCallback(new IMqttActionListener() {
@@ -30,6 +135,7 @@ public class MQTTHelper extends AppCompatActivity {
                 public void onSuccess(IMqttToken asyncActionToken) {
                     // We are connected
                     Log.d(TAG, "onSuccess");
+                    subscribe(client);
                 }
 
                 @Override
@@ -42,10 +148,13 @@ public class MQTTHelper extends AppCompatActivity {
         } catch (MqttException e) {
             e.printStackTrace();
         }
+        finally {
+            return client;
+        }
 
     }
     public void subscribe(MqttAndroidClient client) {
-        String subtopic = "Android";
+        String subtopic = "res";
         int qos = 1;
         try {
             IMqttToken subToken = client.subscribe(subtopic, qos);
